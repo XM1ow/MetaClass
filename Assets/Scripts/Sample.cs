ed90031fb0407d5bc90911e1749596ac67e5c436
XMeow <2224656186@qq.com>
2023-12-07 23:08:47 +0800

    工作流测试-Merge 解决冲突

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -7,7 +7,7 @@ public class Sample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hi Unity!");
+        Debug.Log("hahaha");
     }
 
     // Update is called once per frame
