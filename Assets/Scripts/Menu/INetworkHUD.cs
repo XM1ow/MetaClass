4d0d730edc82622efd3fb86bc399d71de0a8bb9a
XMeow <2224656186@qq.com>
2024-02-01 22:56:41 +0800

    Join Room Button

--- Assets/Scripts/Menu/INetworkHUD.cs
+++ Assets/Scripts/Menu/INetworkHUD.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using DG.Tweening;
 using Mirror;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,23 +11,43 @@ using UnityEngine.UI;
 public class INetworkHUD : MonoBehaviour
 {
     NetworkManager _manager;
-
+    [Header("Buttons")]
     public Button startButton;
     public Button clientButton;
     public Button settingButton;
     public Button quitButton;
-    
+    private Dictionary<Transform, Vector3> _buttonOriginalPositions = new Dictionary<Transform, Vector3>();
+    [Header("Sub Panels")] 
+    public GameObject joinRoomPanel;
+
+    public GameObject settingPanel;
     void Awake()
     {
         _manager = GetComponent<NetworkManager>();
-        if(startButton)
+        if (startButton)
+        {
             startButton.onClick.AddListener(OnStartButton);
+            var t = startButton.transform;
+            _buttonOriginalPositions.Add(t, t.position);
+        }
         if(clientButton)
+        {
             clientButton.onClick.AddListener(OnClientButton);
+            var t = clientButton.transform;
+            _buttonOriginalPositions.Add(t, t.position);
+        }
         if(settingButton)
+        {
             settingButton.onClick.AddListener(OnSettingButton);
+            var t = settingButton.transform;
+            _buttonOriginalPositions.Add(t, t.position);
+        }
         if(quitButton)
+        {
             quitButton.onClick.AddListener(OnQuitButton);
+            var t = quitButton.transform;
+            _buttonOriginalPositions.Add(t, t.position);
+        }
     }
     private void OnStartButton()
     {
@@ -56,9 +78,17 @@ public class INetworkHUD : MonoBehaviour
 
     private void CallClientUI()
     {
-        
-        if(!NetworkClient.isConnected && !NetworkServer.active)
-            _manager.StartClient();
+        foreach (var button in _buttonOriginalPositions.Keys)
+        {
+            button.DOMove(_buttonOriginalPositions[button] + Vector3.left * 200f, 3f);
+        }
+        joinRoomPanel.SetActive(true);
+        /*if(!NetworkClient.isConnected && !NetworkServer.active)
+            _manager.StartClient();*/
     }
-    
+
+    public void StartClientWithIP()
+    {
+        
+    }
 }
