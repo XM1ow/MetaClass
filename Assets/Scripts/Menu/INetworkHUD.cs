7d754bf4e26e5ec22a408835fe42fb4f639b133e
XMeow <2224656186@qq.com>
2024-02-04 21:35:49 +0800

    主页创建房间;加入房间

--- Assets/Scripts/Menu/INetworkHUD.cs
+++ Assets/Scripts/Menu/INetworkHUD.cs
@@ -4,6 +4,7 @@ using DG.Tweening;
 using Mirror;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 [DisallowMultipleComponent]
 [AddComponentMenu("Network/I Network HUD")]
@@ -11,84 +12,94 @@ using UnityEngine.UI;
 public class INetworkHUD : MonoBehaviour
 {
     NetworkManager _manager;
-    [Header("Buttons")]
+    [Header("Main Buttons")]
     public Button startButton;
     public Button clientButton;
     public Button settingButton;
     public Button quitButton;
-    private Dictionary<Transform, Vector3> _buttonOriginalPositions = new Dictionary<Transform, Vector3>();
-    [Header("Sub Panels")] 
+    private GameObject _buttonParent;
+    [Header("Join Room Panel")] 
     public GameObject joinRoomPanel;
 
+    private TMP_InputField _serverAddress;
+    private Button _connectButton;
+    private Button _returnButton;
+    [Header("Settings Panel")]
     public GameObject settingPanel;
     void Awake()
     {
         _manager = GetComponent<NetworkManager>();
+        // Main Panel
+        _buttonParent = GameObject.Find("Buttons");
+        if (_buttonParent)
+        {
+            startButton = _buttonParent.transform.Find("Host Room").GetComponent<Button>();
+            clientButton = _buttonParent.transform.Find("Join Room").GetComponent<Button>();
+            settingButton = _buttonParent.transform.Find("Settings").GetComponent<Button>();
+            quitButton = _buttonParent.transform.Find("Quit").GetComponent<Button>();
+        }
         if (startButton)
         {
-            startButton.onClick.AddListener(OnStartButton);
-            var t = startButton.transform;
-            _buttonOriginalPositions.Add(t, t.position);
+            startButton.onClick.AddListener(() =>
+            {
+                if(!NetworkClient.isConnected && !NetworkServer.active)
+                {
+                    _manager.StartHost(); // client + server
+                    Debug.Log($"Starting server at {NetworkManager.singleton.networkAddress}");
+                }
+            });
         }
         if(clientButton)
         {
-            clientButton.onClick.AddListener(OnClientButton);
-            var t = clientButton.transform;
-            _buttonOriginalPositions.Add(t, t.position);
+            clientButton.onClick.AddListener(CallClientUI);
         }
         if(settingButton)
         {
             settingButton.onClick.AddListener(OnSettingButton);
-            var t = settingButton.transform;
-            _buttonOriginalPositions.Add(t, t.position);
         }
         if(quitButton)
         {
             quitButton.onClick.AddListener(OnQuitButton);
-            var t = quitButton.transform;
-            _buttonOriginalPositions.Add(t, t.position);
         }
-    }
-    private void OnStartButton()
-    {
-        Debug.Log($"start button");
-        if(!NetworkClient.isConnected && !NetworkServer.active)
+        // Join Room Panel
+        if (joinRoomPanel)
         {
-            _manager.StartHost(); // client + server
-            Debug.Log($"Starting server at {NetworkManager.singleton.networkAddress}");
+            _serverAddress = joinRoomPanel.transform.Find("Server Address").GetComponentInChildren<TMP_InputField>();
+            _connectButton = joinRoomPanel.transform.Find("Connect").GetComponent<Button>();
+            _returnButton = joinRoomPanel.transform.Find("Back").GetComponent<Button>();
+            if (_connectButton)
+            {
+                _connectButton.onClick.AddListener(() =>
+                {
+                    _manager.networkAddress = _serverAddress.text == "" ?"localhost" : _serverAddress.text;
+                    _manager.StartClient();
+                    Debug.Log($"Connecting to {_manager.networkAddress}");
+                    joinRoomPanel.SetActive(false);
+                });
+            }
+            if (_returnButton)
+            {
+                _returnButton.onClick.AddListener(() =>
+                {
+                    joinRoomPanel.SetActive(false);
+                    _buttonParent.SetActive(true);
+                });
+            }
         }
     }
-
-    private void OnClientButton()
-    {
-        Debug.Log($"client button");
-        CallClientUI();
-    }
-
     private void OnSettingButton()
     {
-        Debug.Log($"setting button");
     }
 
     private void OnQuitButton()
     {
-        Debug.Log($"quit button");
-
     }
-
+    
     private void CallClientUI()
     {
-        foreach (var button in _buttonOriginalPositions.Keys)
-        {
-            button.DOMove(_buttonOriginalPositions[button] + Vector3.left * 200f, 3f);
-        }
+        _buttonParent.SetActive(false);
         joinRoomPanel.SetActive(true);
-        /*if(!NetworkClient.isConnected && !NetworkServer.active)
-            _manager.StartClient();*/
     }
+    
 
-    public void StartClientWithIP()
-    {
-        
-    }
 }
