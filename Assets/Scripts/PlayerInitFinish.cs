4d8102fded1a115cb1b0ec7c1e5aada0e6cde3a2
XMeow <2224656186@qq.com>
2024-05-02 17:17:24 +0800

    根据自定义角色时的配置创建角色预制体

--- Assets/Scripts/PlayerInitFinish.cs
+++ Assets/Scripts/PlayerInitFinish.cs
@@ -8,27 +8,39 @@ using UnityEngine.SceneManagement;
 
 public class PlayerInitFinish : MonoBehaviour
 {
-    private GameObject man;
+    private GameObject male;
     private GameObject female;
+
+    public GameObject malePrefab;
+    public GameObject femalePrefab;
+
+    public GameObject mainMenu;
+    public GameObject playerCustomization;
 
     private void Start()
     {
         female = transform.GetChild(1).GetChild(0).gameObject;
-        man = transform.GetChild(1).GetChild(1).gameObject;
+        male = transform.GetChild(1).GetChild(1).gameObject;
     }
 
     public void Finish()
     {
         
-        if (man.activeSelf)
+        if (male.activeSelf)
         {
-            man.GetComponent<CharacterCustomization>().SaveParameterJsonString();
+            male.GetComponent<CharacterCustomization>().SaveToJSON();
+            NetworkManager.singleton.playerPrefab = malePrefab;
         }
         else
         {
-            female.GetComponent<CharacterCustomization>().SaveParameterJsonString();
+            female.GetComponent<CharacterCustomization>().SaveToJSON();
+            NetworkManager.singleton.playerPrefab = femalePrefab;
         }
-        Destroy(gameObject);
-        SceneManager.LoadScene("Scenes/Menu");
+        // return to menu
+        if (mainMenu && playerCustomization)
+        {
+            playerCustomization.SetActive(false);
+            mainMenu.SetActive(true);
+        }
     }
 }
